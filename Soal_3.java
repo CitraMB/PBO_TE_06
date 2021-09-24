fb6589b568b55164725b7dcb3e3b0176b4634d28
CitraMB <citra.mutiara123@gmail.com>
2021-09-23 22:52:10 -0700

    menambahkan soal_3

---
+++ Soal_3.java
@@ -0,0 +1,11 @@
+package pboW2;
+
+public class Soal_3 
+{
+	public static void main(String[] args) {
+		double x = 92.98;
+		int nx = (int) Math.round(x); 
+		//Math.round berguna untuk membulatkan nilai ke angka yang terdekat
+		System.out.println(nx);
+		}
+}
