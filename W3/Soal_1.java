c1f9707fc49978422423b7f06c8716db89723d97
CitraMB <citra.mutiara123@gmail.com>
2021-09-30 23:48:46 -0700

    menambahkan soal_1

--- W3/Soal_1.java
+++ W3/Soal_1.java
@@ -1,1 +1,35 @@
-//haloo
+package pboW3;
+
+import java.util.Scanner;
+
+public class Soal_1 {
+
+	public static void main(String[] args) {
+		//masukkan perintah java scanner
+		Scanner in = new Scanner (System.in);
+		String kata = in.nextLine();
+		//mengubah string menjadi karakter array
+		char[] karakter = kata.toCharArray();
+		int k = 0, i = 0; //variabel untuk menghitung kata
+		//looping
+		while ( i < kata.length()) {
+			if (((karakter[i] >= 'A') && (karakter[i] <= 'Z')) || ((karakter[i] >= 'a') && (karakter[i] <= 'z'))) {
+				i++; //menambah indeks array
+			}else {
+				k++; //menambah jika bukan alfabet
+				i++; //menambah indkes array
+			}
+		}
+		System.out.println(k+1);
+		//looping
+		for (i = 0 ; i < kata.length() ; i++) {
+			if (((karakter[i] >= 'A') && (karakter[i] <= 'Z')) || ((karakter[i] >= 'a') && (karakter[i] <= 'z'))) {
+				System.out.print(karakter[i]);
+			}else {
+				//jika bukan alfabet
+				System.out.print("\n");
+			}
+		}
+	}
+
+}
